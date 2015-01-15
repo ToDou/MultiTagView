b08a7aa5a6b5c61a0fb06ebfb1ddae805af21819
ToDou <yytodou123@gmail.com>
2015-01-15 21:55:58 +0800

    add the arraylist

--- app/src/main/java/com/test/tudou/multitagview/MainActivity.java
+++ app/src/main/java/com/test/tudou/multitagview/MainActivity.java
@@ -7,12 +7,32 @@ import android.view.MenuItem;
 
 import com.test.tudou.multitagview.view.MultiTagView;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
     //https://github.com/cloay/CTagView
+
+    MultiTagView multiTagView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        multiTagView = (MultiTagView) findViewById(R.id.tag_view_add_by_list);
+
+        updateView();
+    }
+
+    private void updateView() {
+        multiTagView.setTagClickable(false);
+        multiTagView.setShowAddButton(false);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("苹果");
+        arrayList.add("葡萄");
+        arrayList.add("西红柿");
+        arrayList.add("哈哈哈哈哈");
+        multiTagView.addTags(arrayList);
     }
 }
